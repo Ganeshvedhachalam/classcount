dde8f0158953af437319eadb4105d9bf0bc24157
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-05-19 00:40:39 +0530

    class comp of counter

---
+++ src/counter.js
@@ -0,0 +1,32 @@
+
+
+import React  ,{Component} from  "react"
+
+class Counter extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            count:0
+        }
+    }
+    increment =()=>{
+     this.setState({count:this.state.count+1})
+    }
+    decrement =()=>{
+        this.setState({count:this.state.count-1})
+       }
+
+    render() {
+      return (
+        <div>
+            <h1>Counter App</h1>
+            <h3>count: {this.state.count}</h3>
+            <button style={{padding:"10px",width:"200px",height:"70px" ,margin:"15px",fontWeight:"bolder",fontSize:"20px"}} 
+            onClick={this.increment}>Increment</button>
+            <button style={{padding:"10px",width:"200px",margin:"15px",height :"70px",fontWeight:"bolder",fontSize:"20px"}}
+             onClick={this.decrement}>Decrement</button>
+        </div>
+      )
+    }
+}
+export default Counter;
